a27fd7d65fc2a5efd772e896d9f8361f55e0aa25
cbrit0 <cristobal.brito.p@gmail.com>
2025-01-26 22:25:21 -0300

    exercise 3.2

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+	const requestTime = new Date()
+	response.send(`
+		<p>Phonebook has info for ${persons.length} people</p>
+		<p>Request received at: ${requestTime}</p>
+	`);
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
