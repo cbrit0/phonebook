ea68506013340291827e86c4aa8863da246aa4ca
cbrit0 <cristobal.brito.p@gmail.com>
2025-01-26 23:15:51 -0300

    exercises 3.5-3.6

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json())
+
 let persons = [
   { 
     id: "1",
@@ -53,6 +55,39 @@ app.delete('/api/persons/:id', (request, response) => {
 	persons = persons.filter(person => person.id !== id)
 
 	response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body
+
+	if (!body.name) {
+		return response.status(400).json({
+			error: 'name missing'
+		})
+	}
+
+	if (!body.number) {
+		return response.status(400).json({
+			error: 'number missing'
+		})
+	}
+
+	const duplicate = persons.find(person => person.name === body.name);
+  if (duplicate) {
+    return response.status(400).json({
+			error: 'name must be unique'
+		});
+  }
+
+	const person = {
+		id: Math.floor(Math.random() * 1000000).toString(),
+		name: body.name,
+		number: body.number
+	}
+
+	persons = persons.concat(person)
+
+	response.json(person)
 })
 
 app.listen(PORT, () => {
